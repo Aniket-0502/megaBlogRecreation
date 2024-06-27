f7422a76411df5f9461886d309c7174a9a2bf280
Aniket-0502 <134960557+Aniket-0502@users.noreply.github.com>
2024-06-28 00:16:24 +0530

    updating index.js in components folder

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Container from "../Container/container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+
+export { Container, Logo, LogoutBtn };
--- src/index.js
+++
@@ -1,3 +0,0 @@
-import Container from "./Container/container";
-import Logo from "./components/Logo";
-import LogoutBtn from "./components/Header/LogoutBtn";
