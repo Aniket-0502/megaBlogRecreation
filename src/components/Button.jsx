ac77f8dc497ff174486b99c420ec0493b42c64f5
Aniket-0502 <134960557+Aniket-0502@users.noreply.github.com>
2024-06-28 00:21:02 +0530

    Adding button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+// Creating a button component which would be readily available for use
+
+export default function Button({
+    children,
+    type = "button",
+    bgColor = "bg-blue-600",
+    textColor = "text-white",
+    className = "",
+    ...props
+}) {
+    return (
+        <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>
+            {children}
+        </button>
+    );
+}
