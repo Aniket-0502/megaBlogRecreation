b888cdb4358f5ea7d97fa4ea8708f536889dca69
Aniket-0502 <134960557+Aniket-0502@users.noreply.github.com>
2024-06-29 10:32:44 +0530

    Home Page update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,17 @@
 import React, {useEffect, useState} from 'react'
 import appwriteService from "../appwrite/config";
-import {Container, PostCard} from '../components/index'
-import { useSelector } from 'react-redux';
-import { Query } from 'appwrite';
-import conf from '../conf/conf';
+import {Container, PostCard} from '../components'
 
 function Home() {
     const [posts, setPosts] = useState([])
-    const userStatus = useSelector((state)=>state.auth.status)
-    let userIdfromStore = String(useSelector((state)=>state.auth.userData?.$id))
+
     useEffect(() => {
-        // Only user's posts are visible on Home Page
-        if (userIdfromStore) {
-            appwriteService.getPosts([
-                Query.equal("status", ["active"]),
-                Query.equal("userId", [userIdfromStore])
-            ]).then((posts) => {
-                if (posts) {
-                    setPosts(posts.documents);
-                }
-            });
-        }
-    }, [userIdfromStore]);
-    
-    
+        appwriteService.getPosts().then((posts) => {
+            if (posts) {
+                setPosts(posts.documents)
+            }
+        })
+    }, [])
   
     if (posts.length === 0) {
         return (
@@ -32,7 +20,7 @@ function Home() {
                     <div className="flex flex-wrap">
                         <div className="p-2 w-full">
                             <h1 className="text-2xl font-bold hover:text-gray-500">
-                                {userStatus ? "Create Posts to see" : "Login to read post"}
+                                Login to read posts
                             </h1>
                         </div>
                     </div>
