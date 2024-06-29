3d0c7a6e47b3c09f8137620fb1c15c1c790b6c5e
Aniket-0502 <134960557+Aniket-0502@users.noreply.github.com>
2024-06-29 10:19:49 +0530

    isAuthor

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -11,7 +11,7 @@ export default function Post() {
     const navigate = useNavigate();
 
     const userData = useSelector((state) => state.auth.userData);
-    isAuthor = post && userData ? post.userId === userData.$id : false;
+    const isAuthor = post && userData ? post.userId === userData.$id : false;
     
     
 
