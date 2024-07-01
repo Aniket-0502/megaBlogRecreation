7459810ea53c51b28e660dff95ff2d59fb1d2aa3
Aniket-0502 <134960557+Aniket-0502@users.noreply.github.com>
2024-07-01 17:06:10 +0530

    cloudinary_asset

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,7 +6,7 @@ function Logo({white = false}) {
   return (
     <div className='flex items-center'>
       <div className='m-2'>
-        <img className='h-12 w-12' src='../../favicon/android-chrome-192x192.png' alt="Logo" />
+        <img className='h-12 w-12' src='https://asset.cloudinary.com/dhkl5zhml/fee01a99a385df5ad603a9516b3a29dc' alt="Logo" />
       </div>
       <div>
         <p className={`font-playwriteIt text-[24px] ${reqWhite? "text-white" : "text-black"}`}>BlogVerse</p>
