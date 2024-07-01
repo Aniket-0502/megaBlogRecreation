2b9d5b08facb2396d6275c8e358dcb6f33cd27b4
Aniket-0502 <134960557+Aniket-0502@users.noreply.github.com>
2024-07-01 23:38:36 +0530

    CSS updates

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,143 +1,143 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Logo from '../Logo'
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../Logo";
 
 function Footer() {
   return (
     <section className="relative overflow-hidden py-10 bg-gray-800">
-            <div className="relative z-10 mx-auto max-w-7xl px-4">
-                <div className="-m-6 flex flex-wrap">
-                    <div className="w-full p-6 md:w-1/2 lg:w-5/12">
-                        <div className="flex h-full flex-col justify-between">
-                            <div className="mb-4 inline-flex items-center">
-                                <Logo width="100px" white='true' />
-                            </div>
-                            <div>
-                                <p className="text-sm text-white">
-                                    &copy; Copyright 2024. All Rights Reserved with BlogVerse
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="w-full p-6 md:w-1/2 lg:w-2/12">
-                        <div className="h-full">
-                            <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-white">
-                                Company
-                            </h3>
-                            <ul>
-                                <li className="mb-4">
-                                    <Link
-                                        className=" text-base font-medium text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
-                                        to="/"
-                                    >
-                                        Features
-                                    </Link>
-                                </li>
-                                <li className="mb-4">
-                                    <Link
-                                        className=" text-base font-medium  text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
-                                        to="/"
-                                    >
-                                        Pricing
-                                    </Link>
-                                </li>
-                                <li className="mb-4">
-                                    <Link
-                                        className=" text-base font-medium  text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
-                                        to="/"
-                                    >
-                                        Affiliate Program
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        className=" text-base font-medium  text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
-                                        to="/"
-                                    >
-                                        Press Kit
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="w-full p-6 md:w-1/2 lg:w-2/12">
-                        <div className="h-full">
-                            <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-white">
-                                Support
-                            </h3>
-                            <ul>
-                                <li className="mb-4">
-                                    <Link
-                                        className=" text-base font-medium  text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
-                                        to="/"
-                                    >
-                                        Account
-                                    </Link>
-                                </li>
-                                <li className="mb-4">
-                                    <Link
-                                        className=" text-base font-medium text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
-                                        to="/"
-                                    >
-                                        Help
-                                    </Link>
-                                </li>
-                                <li className="mb-4">
-                                    <Link
-                                        className=" text-base font-medium  text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
-                                        to="/"
-                                    >
-                                        Contact Us
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        className=" text-base font-medium  text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
-                                        to="/"
-                                    >
-                                        Customer Support
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="w-full p-6 md:w-1/2 lg:w-3/12">
-                        <div className="h-full">
-                            <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-white">
-                                Legals
-                            </h3>
-                            <ul>
-                                <li className="mb-4">
-                                    <Link
-                                        className=" text-base font-medium text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
-                                        to="/"
-                                    >
-                                        Terms &amp; Conditions
-                                    </Link>
-                                </li>
-                                <li className="mb-4">
-                                    <Link
-                                        className=" text-base font-medium  text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
-                                        to="/"
-                                    >
-                                        Privacy Policy
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        className=" text-base font-medium  text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
-                                        to="/"
-                                    >
-                                        Licensing
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+      <div className="relative z-10 mx-auto max-w-7xl px-4">
+        <div className="-m-6 flex flex-wrap">
+          <div className="w-[35%] p-6 md:w-1/2 lg:w-5/12">
+            <div className="flex flex-col justify-between">
+              <div className="mb-4 inline-flex items-center">
+                <Logo width="75px" white="true" />
+              </div>
             </div>
-        </section>
-  )
+          </div>
+          <div className="w-full p-6 md:w-1/2 lg:w-2/12">
+            <div className="h-full">
+              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-white">
+                Company
+              </h3>
+              <ul>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
+                    to="/"
+                  >
+                    Features
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium  text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
+                    to="/"
+                  >
+                    Pricing
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium  text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
+                    to="/"
+                  >
+                    Affiliate Program
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className=" text-base font-medium  text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
+                    to="/"
+                  >
+                    Press Kit
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="w-full p-6 md:w-1/2 lg:w-2/12">
+            <div className="h-full">
+              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-white">
+                Support
+              </h3>
+              <ul>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium  text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
+                    to="/"
+                  >
+                    Account
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
+                    to="/"
+                  >
+                    Help
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium  text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
+                    to="/"
+                  >
+                    Contact Us
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className=" text-base font-medium  text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
+                    to="/"
+                  >
+                    Customer Support
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="w-full p-6 md:w-1/2 lg:w-3/12">
+            <div className="h-full">
+              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-white">
+                Legals
+              </h3>
+              <ul>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
+                    to="/"
+                  >
+                    Terms &amp; Conditions
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium  text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
+                    to="/"
+                  >
+                    Privacy Policy
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className=" text-base font-medium  text-white hover:text-red-600 hover:underline decoration-wavy underline-offset-4"
+                    to="/"
+                  >
+                    Licensing
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p className="text-sm text-white mt-8">
+            &copy; Copyright 2024. All Rights Reserved with BlogVerse
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,98 +1,178 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 import appwriteService from "../appwrite/config";
-import {Container, PostCard} from '../components'
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Container, PostCard } from "../components";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 function Home() {
-    const [posts, setPosts] = useState([])
-    const isLoggedIn = useSelector((state)=> state.auth.status)
-    const navigate = useNavigate()
-    useEffect(() => {
-        appwriteService.getPosts().then((posts) => {
-            if (posts) {
-                setPosts(posts.documents)
-            }
-        })
-    }, [])
-  
-    if (!isLoggedIn) {
-        return (
-            <div className="w-full">
-                <Container>
-                    <div>
-                        <div className=' py-8 m-6 mb-[70px] flex flex-wrap justify-center items-center'>
-                            <div className=''>
-                                <img className='h-[436px] w-[655px] rounded-full shadow-slate-400' src="https://images.pexels.com/photos/8367812/pexels-photo-8367812.jpeg" alt="Start Writing Today" />
-                            </div>
-                            <div className='flex flex-col'>
-                                <div className='text-right text-[70px]'>Write Your</div>
-                                <div className='text-right text-[70px] font-bold gradient-text'>Article</div>
-                                <div className='text-right text-[70px]'>here</div>
-                                <div  className='grid place-content-end mt-[20px]'>
-                                    <button onClick={() => navigate('/signup')}>
-                                        <div className='bg-red-500 rounded-full text-center p-3 w-[180px] text-white'>
-                                                Create Account
-                                        </div>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='bg-red-500 mt-4 flex flex-wrap justify-evenly items-center px-7 w-full h-[550px] '>
-                            <div className='font-semibold'>
-                                <h2 className='text-3xl text-white font-playwriteDeGrund my-5 text-center'>
-                                    Hang onto your memories
-                                </h2>
-                            
-                            <p className='text-white text-center w-[300px] font-playwriteDeGrund'>
-                                Save the moments that matter, BlogVerse helps you store thousands of posts and photos
-                            </p>
-                            </div>
-                            <div>
-                                <div className='flex items-center'>
-                                    <img className = 'h-[243px] w-[320px] rotate-12 z-20' src="https://images.pexels.com/photos/2174656/pexels-photo-2174656.jpeg" alt="first" />
-                                    <img className = 'h-[243px] w-[335px] rotate-[-20deg] z-10' src="https://images.pexels.com/photos/1371360/pexels-photo-1371360.jpeg" alt="second" />
-                                    <img className = 'h-[243px] w-[295px] rotate-[18deg] z-0' src="https://images.pexels.com/photos/450441/pexels-photo-450441.jpeg" alt="third" />
-                                </div>
-                            </div>
-                        </div>
-                        <div className='px-7 w-full h-[450px] grid place-content-center text-red-600'>
-                            <div className='flex flex-col items-center'>
-                                <h2 className='font-bold text-4xl text-center font-playwriteDeGrund m-4'>
-                                    Join Millions of Others
-                                </h2>
-                                <p className='text-xl text-center w-[250px] font-playwriteDeGrund m-4'>
-                                    Join BlogVerse to share your unique voice, connect with a vibrant community of bloggers, discover endless inspiration, and stay updated with the latest trends and stories!
-                                </p>
-                                <div className='text-center m-4 rounded-full bg-orange-600 py-4 px-5'>
-                                    <button onClick={() => navigate('/signup')}>
-                                        <div className='text-white'>
-                                            Join Today!
-                                        </div>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </Container>
-            </div>
-        )
-    }
+  const [posts, setPosts] = useState([]);
+  const isLoggedIn = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+  useEffect(() => {
+    appwriteService.getPosts().then((posts) => {
+      if (posts) {
+        setPosts(posts.documents);
+      }
+    });
+  }, []);
+
+  const [index, setIndex] = useState(0);
+  const images = [
+    "https://images.pexels.com/photos/2174656/pexels-photo-2174656.jpeg",
+    "https://images.pexels.com/photos/1371360/pexels-photo-1371360.jpeg",
+    "https://images.pexels.com/photos/450441/pexels-photo-450441.jpeg",
+  ];
+  const intervalTime = 3000; // 3 seconds
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, intervalTime);
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, [images.length]);
+
+  const prevSlide = () => {
+    setIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
+  const nextSlide = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+  if (!isLoggedIn) {
     return (
-        // <div className='w-full py-8'>
-        //     <Container>
-        //         <div className='flex flex-wrap'>
-        //             {posts.map((post) => (
-        //                 <div key={post.$id} className='p-2 w-1/4'>
-        //                     <PostCard {...post} />
-        //                 </div>
-        //             ))}
-        //         </div>
-        //     </Container>
-        // </div>
-        <div>
-            Home After Login
-        </div>
-    )
+      <div className="w-full">
+        <Container>
+          <div>
+            <div className=" py-8 m-6 mb-[70px] flex flex-wrap justify-center items-center">
+              <div className="">
+                <img
+                  className=" hidden md:block h-[436px] w-[655px] rounded-full shadow-slate-400"
+                  src="https://images.pexels.com/photos/8367812/pexels-photo-8367812.jpeg"
+                  alt="Start Writing Today"
+                />
+                <img
+                  className="md:hidden h-[256px] w-[256px] rounded-full shadow-slate-400 object-fill"
+                  src="https://images.pexels.com/photos/8367812/pexels-photo-8367812.jpeg"
+                  alt="Start Writing Today"
+                />
+              </div>
+              <div className="flex flex-col">
+                <div className="text-center text-[35px] lg:text-right lg:text-[70px]">
+                  Write Your
+                </div>
+                <div className="text-center text-[35px] lg:text-right lg:text-[70px] font-bold gradient-text">
+                  Article
+                </div>
+                <div className="text-center text-[35px] lg:text-right lg:text-[70px]">
+                  here
+                </div>
+                <div className="grid place-content-center lg:place-content-end mt-[20px]">
+                  <button onClick={() => navigate("/signup")}>
+                    <div className="bg-red-500 rounded-full text-center p-3 w-[180px] text-white">
+                      Create Account
+                    </div>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="bg-red-500 mt-4 flex flex-wrap justify-evenly items-end lg:items-center w-full h-[550px] ">
+              <div className="font-semibold mx-7">
+                <h2 className="text-3xl text-white font-playwriteDeGrund my-5 text-center">
+                  Hang onto your memories
+                </h2>
+
+                <p className="text-white text-center w-[300px] font-playwriteDeGrund">
+                  Save the moments that matter, BlogVerse helps you store
+                  thousands of posts and photos
+                </p>
+              </div>
+              <div>
+                <div className="flex items-center mr-7">
+                  <img
+                    className="hidden lg:block h-[243px] w-[320px] rotate-12 z-20"
+                    src="https://images.pexels.com/photos/2174656/pexels-photo-2174656.jpeg"
+                    alt="first"
+                  />
+                  <img
+                    className="hidden lg:block h-[243px] w-[335px] rotate-[-20deg] z-10"
+                    src="https://images.pexels.com/photos/1371360/pexels-photo-1371360.jpeg"
+                    alt="second"
+                  />
+                  <img
+                    className="hidden lg:block h-[243px] w-[295px] rotate-[18deg] z-0"
+                    src="https://images.pexels.com/photos/450441/pexels-photo-450441.jpeg"
+                    alt="third"
+                  />
+                </div>
+                <div className="lg:hidden carousel overflow-hidden relative w-full">
+                  <div
+                    className="carousel-inner flex transition-transform duration-500 ease-in-out"
+                    style={{ transform: `translateX(-${index * 100}%)` }}
+                  >
+                    {images.map((image, i) => (
+                      <div
+                        className="carousel-item w-full flex-shrink-0"
+                        key={i}
+                      >
+                        <img
+                          src={image}
+                          alt={`Image ${i + 1}`}
+                          className="w-[100vw] h-[62vh] mt-4"
+                        />
+                      </div>
+                    ))}
+                  </div>
+                  <button
+                    className="text-white absolute top-1/2 left-2 transform -translate-y-1/2 bg-transparent hover:bg-red-500 px-4 py-2 rounded-full"
+                    onClick={prevSlide}
+                  >
+                    &larr;
+                  </button>
+                  <button
+                    className="text-white absolute top-1/2 right-4 transform -translate-y-1/2 bg-transparent hover:bg-red-500 px-4 py-2 rounded-full"
+                    onClick={nextSlide}
+                  >
+                    &rarr;
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="px-7 w-full h-[450px] grid place-content-center text-red-600">
+              <div className="flex flex-col items-center">
+                <h2 className="font-bold text-4xl text-center font-playwriteDeGrund m-4">
+                  Join Millions of Others
+                </h2>
+                <p className="text-xl text-center w-[250px] font-playwriteDeGrund m-4">
+                  Join BlogVerse to share your unique voice, connect with a
+                  vibrant community of bloggers, discover endless inspiration,
+                  and stay updated with the latest trends and stories!
+                </p>
+                <div className="text-center m-4 rounded-full bg-orange-600 py-4 px-5">
+                  <button onClick={() => navigate("/signup")}>
+                    <div className="text-white">Join Today!</div>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+  return (
+    // <div className='w-full py-8'>
+    //     <Container>
+    //         <div className='flex flex-wrap'>
+    //             {posts.map((post) => (
+    //                 <div key={post.$id} className='p-2 w-1/4'>
+    //                     <PostCard {...post} />
+    //                 </div>
+    //             ))}
+    //         </div>
+    //     </Container>
+    // </div>
+    <div>Home After Login</div>
+  );
 }
 
-export default Home
+export default Home;
